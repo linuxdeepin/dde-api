f5c3b7fa5fa7497eae4ea0c81c52379b5bceb117
jouyouyun <jouyouwen717@gmail.com>
2014-03-24 09:55:58 +0800

    mousearea: fix the key/button event do not send a signal

--- mousearea/mouse_event.go
+++ mousearea/mouse_event.go
@@ -26,123 +26,135 @@ package main
 // #include "mouse_record.h"
 import "C"
 
+import "fmt"
+
 type coordinateRange struct {
-	X1 int32
-	X2 int32
-	Y1 int32
-	Y2 int32
+        X1      int32
+        X2      int32
+        Y1      int32
+        Y2      int32
 }
 
 //export parseMotionEvent
 func parseMotionEvent(_x, _y int32) {
-	coorX := int32(_x)
-	coorY := int32(_y)
+        coorX := int32(_x)
+        coorY := int32(_y)
 
-	inList, outList := getIDList(coorX, coorY)
-	for _, cookie := range inList {
-		opMouse.MotionCoordinate("inside", coorX, coorY, cookie)
-	}
-	for _, cookie := range outList {
-		opMouse.MotionCoordinate("outside", coorX, coorY, cookie)
-	}
+        inList, outList := getIDList(coorX, coorY)
+        for _, cookie := range inList {
+                if array, ok := idRangeMap[cookie]; ok {
+                        /* moveFlag == 1 : mouse move in area */
+                        if array.moveFlag != 1 {
+                                array.moveFlag = 1
+                                opMouse.MotionCoordinate("inside",
+                                        coorX, coorY, cookie)
+                        }
+                }
+        }
+        for _, cookie := range outList {
+                if array, ok := idRangeMap[cookie]; ok {
+                        /* moveFlag == 0 : mouse move out area */
+                        if array.moveFlag != 0 {
+                                array.moveFlag = 0
+                                opMouse.MotionCoordinate("outside",
+                                        coorX, coorY, cookie)
+                        }
+                }
+        }
 }
 
 //export parseButtonEvent
 func parseButtonEvent(_type, _x, _y int32) {
-	coorX := int32(_x)
-	coorY := int32(_y)
-	tmp := int32(_type)
-	coorType := ""
-	if tmp == C.BUTTON_PRESS {
-		coorType = "Press"
-	} else {
-		coorType = "Release"
-	}
-	cookies, _ := getIDList(coorX, coorY)
-	for _, cookie := range cookies {
-		opMouse.ButtonCoordinate(coorType, coorX, coorY, cookie)
-	}
+        coorX := int32(_x)
+        coorY := int32(_y)
+        tmp := int32(_type)
+        coorType := ""
+        if tmp == C.BUTTON_PRESS {
+                coorType = "Press"
+        } else {
+                coorType = "Release"
+        }
+        cookies, _ := getIDList(coorX, coorY)
+        fmt.Println("Button Cookies: ", cookies)
+        fmt.Printf("\tX: %d, Y: %d\n\n", coorX, coorY)
+        for _, cookie := range cookies {
+                opMouse.ButtonCoordinate(coorType, coorX, coorY, cookie)
+        }
 }
 
 //export parseKeyboardEvent
 func parseKeyboardEvent(_type, _x, _y int32) {
-	coorX := int32(_x)
-	coorY := int32(_y)
-	tmp := int32(_type)
-	coorType := ""
-	if tmp == C.KEY_PRESS {
-		coorType = "Press"
-	} else {
-		coorType = "Release"
-	}
+        coorX := int32(_x)
+        coorY := int32(_y)
+        tmp := int32(_type)
+        coorType := ""
+        if tmp == C.KEY_PRESS {
+                coorType = "Press"
+        } else {
+                coorType = "Release"
+        }
 
-	cookies, _ := getIDList(coorX, coorY)
-	for _, cookie := range cookies {
-		opMouse.KeyboardCoordinate(coorType, coorX, coorY, cookie)
-	}
+        cookies, _ := getIDList(coorX, coorY)
+        fmt.Println("Keyboard Cookies: ", cookies)
+        fmt.Printf("\tX: %d, Y: %d\n\n", coorX, coorY)
+        for _, cookie := range cookies {
+                opMouse.KeyboardCoordinate(coorType, coorX, coorY, cookie)
+        }
 }
 
 func cancleAllReigsterArea() {
-	list := []int32{}
+        list := []int32{}
 
-	for id, _ := range idRangeMap {
-		list = append(list, id)
-		delete(idRangeMap, id)
-	}
+        for id, _ := range idRangeMap {
+                list = append(list, id)
+                delete(idRangeMap, id)
+        }
 
-	println("map len:", len(idRangeMap))
-	for _, cookie := range list {
-		opMouse.CancleAllArea(1365, 767, cookie)
-	}
+        println("map len:", len(idRangeMap))
+        for _, cookie := range list {
+                opMouse.CancleAllArea(1365, 767, cookie)
+        }
 }
 
 func getIDList(x, y int32) ([]int32, []int32) {
-	inList := []int32{}
-	outList := []int32{}
+        inList := []int32{}
+        outList := []int32{}
 
-	for id, array := range idRangeMap {
-		inFlag := false
-		for _, area := range array.areas {
-			if isInArea(x, y, area) {
-				inFlag = true
-				/* moveFlag == 1 : mouse move in area */
-				if array.moveFlag != 1 {
-					array.moveFlag = 1
-					if !isInIDList(id, inList) {
-						inList = append(inList, id)
-					}
-				}
-			}
-		}
-		if !inFlag {
-			/* moveFlag == 0 : mouse move out area */
-			if array.moveFlag != 0 {
-				array.moveFlag = 0
-				if !isInIDList(id, outList) {
-					outList = append(outList, id)
-				}
-			}
-		}
-	}
+        for id, array := range idRangeMap {
+                inFlag := false
+                for _, area := range array.areas {
+                        if isInArea(x, y, area) {
+                                inFlag = true
+                                if !isInIDList(id, inList) {
+                                        inList = append(inList, id)
+                                }
+                        }
+                }
+                if !inFlag {
+                        if !isInIDList(id, outList) {
+                                outList = append(outList, id)
+                        }
+                }
+        }
 
-	return inList, outList
+        return inList, outList
 }
 
 func isInArea(x, y int32, area coordinateRange) bool {
-	if (x >= area.X1 && x <= area.X2) &&
-		(y >= area.Y1 && y <= area.Y2) {
-		return true
-	}
+        if (x >= area.X1 && x <= area.X2) &&
+                (y >= area.Y1 && y <= area.Y2) {
+                return true
+        }
 
-	return false
+        return false
 }
 
 func isInIDList(id int32, list []int32) bool {
-	for _, v := range list {
-		if id == v {
-			return true
-		}
-	}
+        for _, v := range list {
+                if id == v {
+                        return true
+                }
+        }
 
-	return false
+        return false
 }
