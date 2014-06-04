985efd5b0d0e3909bc49cf5a13bc36fe9261f89a
jouyouyun <jouyouwen717@gmail.com>
2014-06-04 10:43:57 +0800

    setdate: delete ntp

--- set-date-time/set_date_time.go
+++ set-date-time/set_date_time.go
@@ -4,7 +4,6 @@ import (
 	"dlib"
 	"dlib/dbus"
 	dlogger "dlib/logger"
-	"net"
 	"os"
 	"os/exec"
 	"strconv"
@@ -12,19 +11,16 @@ import (
 )
 
 const (
-	_NTP_HOST           = "0.pool.ntp.org"
 	_SET_DATE_TIME_DEST = "com.deepin.api.SetDateTime"
 	_SET_DATE_TIME_PATH = "/com/deepin/api/SetDateTime"
 	_SET_DATA_TIME_IFC  = "com.deepin.api.SetDateTime"
 )
 
 var (
-	logger   = dlogger.NewLogger("dde-api/set-date-time")
-	quitChan chan bool
+	logger = dlogger.NewLogger("dde-api/set-date-time")
 )
 
 type SetDateTime struct {
-	ntpRunFlag      bool
 	GenLocaleStatus func(bool, string)
 }
 
@@ -80,60 +76,8 @@ func (sdt *SetDateTime) SetTimezone(tz string) bool {
 	return setTimezone(tz)
 }
 
-func (sdt *SetDateTime) SyncNtpTime() bool {
-	for i := 0; i < 10; i++ {
-		t, err := GetNtpNow()
-		if err == nil && t != nil {
-			dStr, tStr := GetDateTimeAny(t)
-			logger.Infof("Data: %s, Time: %s", dStr, tStr)
-			sdt.SetCurrentDate(dStr)
-			sdt.SetCurrentTime(tStr)
-			return true
-		} else {
-			logger.Info(err.Error())
-			//return false
-		}
-	}
-
-	return false
-}
-
-func (sdt *SetDateTime) SetNtpUsing(using bool) bool {
-	if using {
-		if sdt.ntpRunFlag {
-			sdt.SyncNtpTime()
-			logger.Info("Ntp is running....")
-			return true
-		}
-
-		sdt.ntpRunFlag = true
-		go SetNtpThread(sdt)
-	} else {
-		if sdt.ntpRunFlag {
-			logger.Info("Ntp will quit....")
-			quitChan <- true
-		}
-
-		sdt.ntpRunFlag = false
-	}
-	return true
-}
-
-func SetNtpThread(sdt *SetDateTime) {
-	for {
-		sdt.SyncNtpTime()
-		timer := time.NewTimer(time.Minute * 1)
-		select {
-		case <-timer.C:
-		case <-quitChan:
-			return
-		}
-	}
-}
-
 func NewSetDateTime() *SetDateTime {
 	sdt := SetDateTime{}
-	sdt.ntpRunFlag = false
 
 	return &sdt
 }
@@ -160,49 +104,6 @@ func GetDateTimeAny(t *time.Time) (dStr, tStr string) {
 	return dStr, tStr
 }
 
-func GetNtpNow() (*time.Time, error) {
-	raddr, err := net.ResolveUDPAddr("udp", _NTP_HOST+":123")
-	if err != nil {
-		return nil, err
-	}
-
-	data := make([]byte, 48)
-	data[0] = 3<<3 | 3
-
-	con, err := net.DialUDP("udp", nil, raddr)
-	if err != nil {
-		return nil, err
-	}
-
-	defer con.Close()
-
-	_, err = con.Write(data)
-	if err != nil {
-		return nil, err
-	}
-
-	con.SetDeadline(time.Now().Add(5 * time.Second))
-
-	_, err = con.Read(data)
-	if err != nil {
-		return nil, err
-	}
-
-	var sec, frac uint64
-	sec = uint64(data[43]) | uint64(data[42])<<8 | uint64(data[41])<<16 |
-		uint64(data[40])<<24
-	frac = uint64(data[47]) | uint64(data[46])<<8 | uint64(data[45])<<16 |
-		uint64(data[44])<<24
-
-	nsec := sec * 1e9
-	nsec += (frac * 1e9) >> 32
-
-	t := time.Date(1900, 1, 1, 0, 0, 0, 0, time.UTC).
-		Add(time.Duration(nsec)).Local()
-
-	return &t, nil
-}
-
 func stringInSlice(a string, list []string) bool {
 	for _, b := range list {
 		if b == a {
@@ -226,14 +127,26 @@ func main() {
 		logger.SetLogLevel(dlogger.LEVEL_DEBUG)
 	}
 
-	quitChan = make(chan bool)
 	sdt := NewSetDateTime()
 	err := dbus.InstallOnSystem(sdt)
 	if err != nil {
 		panic(err)
 	}
 	dbus.DealWithUnhandledMessage()
-	//select {}
+
+	dbus.SetAutoDestroyHandler(time.Second*1, func() bool {
+		if genLcStart {
+			if genLcEnd {
+				genLcEnd = false
+				genLcStart = false
+				return true
+			}
+		} else {
+			return true
+		}
+
+		return false
+	})
 	if err = dbus.Wait(); err != nil {
 		logger.Error("lost dbus session:", err)
 		os.Exit(1)
