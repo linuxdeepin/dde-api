2f80200b3618196e9948925eb889ff1bf8358969
jouyouyun <jouyouwen717@gmail.com>
2015-08-12 11:15:05 +0800

    thumbnails: optimize text generator referred kde
    Change-Id: I934e4596fa9cedb238aeecffae6c87e1fe60b111

--- thumbnails/text/text.h
+++ thumbnails/text/text.h
@@ -1,6 +1,17 @@
 #ifndef __TEXT_H__
 #define __TEXT_H__
 
-int do_gen_thumbnail(char** text, char* dest);
+typedef struct _THUMB_INFO
+{
+    int width;
+    int height;
+    int xborder;
+    int yborder;
+    int canvasWidth;
+    int canvasHeight;
+    int fontSize;
+} ThumbInfo;
+
+int do_gen_thumbnail(char** text, char* dest, ThumbInfo* info);
 
 #endif
