237fe0a233cfd2f266423cb20a4f9f51a9993b2a
jouyouyun <jouyouwen717@gmail.com>
2015-08-18 17:13:17 +0800

    thumbnails: fixed image with white border after scaled
    Change-Id: If141d88554104a0b7066fa68046288379dc1de61

--- thumbnails/images/scale.go
+++ thumbnails/images/scale.go
@@ -1,24 +1,10 @@
 package images
 
 import (
-	"github.com/disintegration/imaging"
-	"image"
-	"image/color"
-	"runtime/debug"
+	"pkg.deepin.io/lib/graphic"
 )
 
 func Scale(src, dest string, width, height int) error {
-	img, err := imaging.Open(src)
-	if err != nil {
-		return err
-	}
-	defer debug.FreeOSMemory()
-
-	//thumb := imaging.Thumbnail(img, width, height, imaging.Box)
-	thumb := imaging.Fit(img, width, height, imaging.Lanczos)
-
-	dst := imaging.New(width, height, color.NRGBA{0, 0, 0, 0})
-	dst = imaging.Paste(dst, thumb, image.Pt(0, 0))
-
-	return imaging.Save(dst, dest)
+	return graphic.ThumbnailImage(src, dest, width, height,
+		graphic.FormatPng)
 }
