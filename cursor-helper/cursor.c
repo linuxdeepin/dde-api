34e2ee429dfc9c036da6c9d2883027c6da842e2b
jouyouyun <jouyouwen717@gmail.com>
2015-12-11 13:27:43 +0800

    cursor-helper: don't listen gtk cursor changed
    The cursor changed listener should be created by caller.
    Change-Id: I0d4119af3e7e0943a6d8cef5cdad09c8eaa32a04

--- cursor-helper/cursor.c
+++ cursor-helper/cursor.c
@@ -3,10 +3,6 @@
 
 #include "cursor.h"
 
-static void update_gtk_cursor();
-
-static int sig_id = 0;
-
 int
 init_gtk()
 {
@@ -24,37 +20,8 @@ init_gtk()
 }
 
 void
-handle_gtk_cursor_changed()
-{
-	GtkSettings* s = gtk_settings_get_default();
-	sig_id = g_signal_connect(s, "notify::gtk-cursor-theme-name",
-							  update_gtk_cursor, NULL);
-	if (sig_id <= 0) {
-		return;
-	}
-
-	gtk_main();
-}
-
-void
 set_gtk_cursor(char* name)
 {
 	GtkSettings* s = gtk_settings_get_default();
 	g_object_set(G_OBJECT(s), "gtk-cursor-theme-name", name, NULL);
-}
-
-static void
-update_gtk_cursor()
-{
-	GtkSettings* s = gtk_settings_get_default();
-	g_signal_handler_disconnect(s, sig_id);
-	sig_id = 0;
-
-	GdkCursor* cursor = gdk_cursor_new_for_display(
-		gdk_display_get_default(),
-		GDK_LEFT_PTR);
-	gdk_window_set_cursor(gdk_get_default_root_window(), cursor);
-	g_object_unref(G_OBJECT(cursor));
-
-	gtk_main_quit();
 }
