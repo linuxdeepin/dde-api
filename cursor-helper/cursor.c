8025b63d41d4687ed2ed1450351a3b78ced567ff
jouyouyun <jouyouwen717@gmail.com>
2016-04-18 10:46:32 +0800

    cursor-helper: Free cursor
    Change-Id: I2132154034bba6238f122f0ea303aecf6012f795

--- cursor-helper/cursor.c
+++ cursor-helper/cursor.c
@@ -15,22 +15,22 @@
 int
 init_gtk()
 {
-	static gboolean xcb_init = FALSE;
+    static gboolean xcb_init = FALSE;
 
-	if (!xcb_init) {
-		XInitThreads();
+    if (!xcb_init) {
+        XInitThreads();
 
-		if (!gtk_init_check(NULL, NULL)) {
-			return -1;
-		}
-	}
-	xcb_init = TRUE;
-	return 0;
+        if (!gtk_init_check(NULL, NULL)) {
+            return -1;
+        }
+    }
+    xcb_init = TRUE;
+    return 0;
 }
 
 void
 set_gtk_cursor(char* name)
 {
-	GtkSettings* s = gtk_settings_get_default();
-	g_object_set(G_OBJECT(s), "gtk-cursor-theme-name", name, NULL);
+    GtkSettings* s = gtk_settings_get_default();
+    g_object_set(G_OBJECT(s), "gtk-cursor-theme-name", name, NULL);
 }
