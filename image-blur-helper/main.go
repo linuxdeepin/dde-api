7be44d53ca177b00aceccc2a8c2eca1f14aebea1
Hualet Wang <mr.asianwang@gmail.com>
2016-09-06 12:58:32 +0800

    Fix execution of blur_image always return failure
    Change-Id: I5a9d31f668d79dc62509b00b62fb138e2551d715

--- image-blur-helper/main.go
+++ image-blur-helper/main.go
@@ -27,10 +27,10 @@ const (
 var (
 	force      = kingpin.Flag("force", "Force to blur image").Short('f').Default("false").Bool()
 	radius     = kingpin.Flag("radius", "The radius [3 - 49], must odd number(default 39)").Short('r').Default("39").Uint8()
-	rounds     = kingpin.Flag("rounds", "The number of round(default 10).").Short('p').Default("10").Uint64()
+	rounds     = kingpin.Flag("rounds", "The number of round(default 14).").Short('p').Default("14").Uint64()
 	sigma      = kingpin.Flag("sigma", "The blur sigma(default 20.0).").Short('S').Default("20.0").Float64()
-	saturation = kingpin.Flag("saturation", "Multiple current saturation(default 2.0)").Short('s').Default("2.0").Float64()
-	lightness  = kingpin.Flag("lightness", "Multiple current lightness(HSL)(default 0.6)").Short('l').Default("0.6").Float64()
+	saturation = kingpin.Flag("saturation", "Multiple current saturation(default 1.5)").Short('s').Default("1.5").Float64()
+	lightness  = kingpin.Flag("lightness", "Multiple current lightness(HSL)(default 0.9)").Short('l').Default("0.9").Float64()
 	src        = kingpin.Arg("src", "The src file, may be directory").String()
 )
 
@@ -64,7 +64,7 @@ func main() {
 			continue
 		}
 
-		cmd := fmt.Sprintf("exec blur_image -b -s %v -r %v -p %v %q -o %s", *lightness, *saturation, *radius, *rounds, image, dest)
+		cmd := fmt.Sprintf("exec blur_image -l %v -s %v -r %v -p %v %q -o %s", *lightness, *saturation, *radius, *rounds, image, dest)
 		out, err := exec.Command("/bin/sh", "-c", cmd).CombinedOutput()
 		if err != nil {
 			fmt.Printf("Blur '%s' via 'blur_image' failed: %v, %v, try again...\n", image, string(out), err)
