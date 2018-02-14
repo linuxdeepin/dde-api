c0a16eeb61271de7b9e7262efbf17d7fa19d75fe
electricface <songwentai@deepin.com>
2018-02-14 21:15:15 +0800

    feat(sound-theme-player): use newly lib dbusutil
    Change-Id: I1c26519b358095bf32bbedf4465f4c7f8c19c892

--- sound-theme-player/main.go
+++ sound-theme-player/main.go
@@ -20,39 +20,51 @@
 package main
 
 import (
-	"fmt"
 	"time"
 
 	"sync"
 
-	"pkg.deepin.io/lib/dbus"
+	"errors"
+
+	"pkg.deepin.io/lib/dbus1"
+	"pkg.deepin.io/lib/dbusutil"
 	"pkg.deepin.io/lib/log"
 	"pkg.deepin.io/lib/sound_effect"
+)
+
+const (
+	dbusServiceName = "com.deepin.api.SoundThemePlayer"
+	dbusPath        = "/com/deepin/api/SoundThemePlayer"
+	dbusInterface   = dbusServiceName
+)
+
+var (
+	logger = log.NewLogger("sound-theme-player")
 )
 
 type Manager struct {
 	playing bool
 	mu      sync.Mutex
 	player  *sound_effect.Player
+	service *dbusutil.Service
+
+	methods *struct {
+		Play func() `in:"theme,event,device"`
+	}
 }
 
-var (
-	logger = log.NewLogger("sound-theme-player")
-)
-
-func (m *Manager) Play(theme, event, device string) error {
+func (m *Manager) Play(theme, event, device string) *dbus.Error {
 	if theme == "" || event == "" {
-		return fmt.Errorf("invalid theme or event")
+		return dbusutil.ToError(errors.New("invalid theme or event"))
 	}
 	go m.doPlaySound(theme, event, device)
 	return nil
 }
 
-func (*Manager) GetDBusInfo() dbus.DBusInfo {
-	return dbus.DBusInfo{
-		Dest:       "com.deepin.api.SoundThemePlayer",
-		ObjectPath: "/com/deepin/api/SoundThemePlayer",
-		Interface:  "com.deepin.api.SoundThemePlayer",
+func (m *Manager) GetDBusExportInfo() dbusutil.ExportInfo {
+	return dbusutil.ExportInfo{
+		Path:      dbusPath,
+		Interface: dbusInterface,
 	}
 }
 
@@ -79,22 +91,38 @@ func (m *Manager) canQuit() bool {
 	return !playing
 }
 
+func newManager(service *dbusutil.Service) *Manager {
+	player := sound_effect.NewPlayer(false, sound_effect.PlayBackendALSA)
+	return &Manager{
+		player:  player,
+		service: service,
+	}
+}
+
 func main() {
 	logger.Info("start sound-theme-player")
-	var m = new(Manager)
-	m.player = sound_effect.NewPlayer(false, sound_effect.PlayBackendALSA)
-
-	err := dbus.InstallOnSystem(m)
+	service, err := dbusutil.NewSystemService()
 	if err != nil {
-		logger.Error("Install sound player bus failed:", err)
-		return
+		logger.Fatal("failed to new system service", err)
 	}
-	dbus.DealWithUnhandledMessage()
 
-	dbus.SetAutoDestroyHandler(time.Second*10, m.canQuit)
-
-	err = dbus.Wait()
+	hasOwner, err := service.NameHasOwner(dbusServiceName)
 	if err != nil {
-		logger.Error("Lost system bus:", err)
+		logger.Fatal("failed to call NameHasOwner:", err)
 	}
+	if hasOwner {
+		logger.Fatalf("name %q already has the owner", dbusServiceName)
+	}
+
+	m := newManager(service)
+	err = service.Export(m)
+	if err != nil {
+		logger.Fatal("failed to export:", err)
+	}
+	err = service.RequestName(dbusServiceName)
+	if err != nil {
+		logger.Fatal("failed to request name:", err)
+	}
+	service.SetAutoQuitHandler(time.Second*10, m.canQuit)
+	service.Wait()
 }
