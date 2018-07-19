983d1278968c18e78e8cad4757b019f53710aa12
transifex <transifex@linuxdeepin.com>
2018-07-19 12:34:32 +0000

    auto sync po files from transifex
    Change-Id: Iab3f86bf6db45b0ddc07a3a82550ea1b7c6b51c1

---
+++ misc/ts/com.deepin.api.locale-helper.policy/policy_ko.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ko" version="2.1">
+	<context>
+		<name>policy</name>
+		<message>
+			<location filename="com.deepin.api.locale-helper.manage-locale!message" line="0"/>
+			<source>Authentication is required to switch language</source>
+			<translation>언어를 전환하려면 인증이 필요합니다.</translation>
+		</message>
+		<message>
+			<location filename="com.deepin.api.locale-helper.manage-locale!description" line="0"/>
+			<source>Manage your system locale</source>
+			<translation>시스템 로케일 관리</translation>
+		</message>
+	</context>
+</TS>
