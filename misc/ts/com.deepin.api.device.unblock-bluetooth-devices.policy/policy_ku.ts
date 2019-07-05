7bc3d01116ef1f66a1ee5989ff08fdc9de28daf6
electricface1 <songwentai@deepin.com>
2019-07-05 07:04:47 +0000

    chore: auto pull translation files from transifex

--- misc/ts/com.deepin.api.device.unblock-bluetooth-devices.policy/policy_ku.ts
+++ misc/ts/com.deepin.api.device.unblock-bluetooth-devices.policy/policy_ku.ts
@@ -1,15 +1,15 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="ar" version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ku" version="2.1">
 	<context>
 		<name>policy</name>
 		<message>
 			<location filename="com.deepin.api.device.unblock-bluetooth-devices!message" line="0"/>
 			<source>Authentication is required to unblock bluetooth devices</source>
-			<translation>المصادقة مطلوبة لتفعيل أجهزة البلوتوث</translation>
+			<translation>Ji bo rakirina astengiyê ya amûrên bluetoothê rastandina nasnameyê pêwîst e</translation>
 		</message>
 		<message>
 			<location filename="com.deepin.api.device.unblock-bluetooth-devices!description" line="0"/>
 			<source>Unblock bluetooth devices</source>
-			<translation>تفعيل أجهزة البلوتوث</translation>
+			<translation>Astengiya amûrên bluetoothê rake</translation>
 		</message>
 	</context>
 </TS>
--- misc/ts/com.deepin.api.locale-helper.policy/policy_ku.ts
+++ misc/ts/com.deepin.api.locale-helper.policy/policy_ku.ts
@@ -1,15 +1,15 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="fi" version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ku" version="2.1">
 	<context>
 		<name>policy</name>
 		<message>
 			<location filename="com.deepin.api.locale-helper.manage-locale!message" line="0"/>
 			<source>Authentication is required to switch language</source>
-			<translation>Kielen vaihtaminen vaatii tunnistautumisen</translation>
+			<translation>Ji bo guherandina zimanê rastandina nasnameyê pêwîst e</translation>
 		</message>
 		<message>
 			<location filename="com.deepin.api.locale-helper.manage-locale!description" line="0"/>
 			<source>Manage your system locale</source>
-			<translation>Hallitse järjestelmän kieliasetuksia</translation>
+			<translation>Pergala xwe ya cîgehî bi rê ve bibin</translation>
 		</message>
 	</context>
 </TS>
