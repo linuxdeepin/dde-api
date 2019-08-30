eda710f3e768f2d12d978de4c1f44f4156f9404e
electricface1 <songwentai@deepin.com>
2019-08-30 06:25:55 +0000

    chore: auto pull translation files from transifex

---
+++ misc/ts/com.deepin.api.device.unblock-bluetooth-devices.policy/policy_sv.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="sv" version="2.1">
+	<context>
+		<name>policy</name>
+		<message>
+			<location filename="com.deepin.api.device.unblock-bluetooth-devices!message" line="0"/>
+			<source>Authentication is required to unblock bluetooth devices</source>
+			<translation>Autentisering krävs för att avblockera bluetooth</translation>
+		</message>
+		<message>
+			<location filename="com.deepin.api.device.unblock-bluetooth-devices!description" line="0"/>
+			<source>Unblock bluetooth devices</source>
+			<translation>Avblockera bluetooth enheter</translation>
+		</message>
+	</context>
+</TS>
--- misc/ts/com.deepin.api.locale-helper.policy/policy_sv.ts
+++ misc/ts/com.deepin.api.locale-helper.policy/policy_sv.ts
@@ -1,15 +1,15 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="pt" version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="sv" version="2.1">
 	<context>
 		<name>policy</name>
 		<message>
 			<location filename="com.deepin.api.locale-helper.manage-locale!message" line="0"/>
 			<source>Authentication is required to switch language</source>
-			<translation>A autenticação é necessária para alterar o idioma</translation>
+			<translation>Autentisering krävs för att byta språk</translation>
 		</message>
 		<message>
 			<location filename="com.deepin.api.locale-helper.manage-locale!description" line="0"/>
 			<source>Manage your system locale</source>
-			<translation>Gerir a localização do seu sistema</translation>
+			<translation>Hantera systemets språk</translation>
 		</message>
 	</context>
 </TS>
