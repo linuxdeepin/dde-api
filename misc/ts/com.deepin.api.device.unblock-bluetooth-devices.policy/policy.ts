a00b050f999d546c43a42f7345a96752e5eb3224
songwentai <songwentai@deepin.com>
2019-09-28 01:16:31 +0000

    chore: auto pull translation files from transifex

--- misc/ts/com.deepin.api.device.unblock-bluetooth-devices.policy/policy.ts
+++ misc/ts/com.deepin.api.device.unblock-bluetooth-devices.policy/policy.ts
@@ -1,17 +1,15 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE TS>
-<TS version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="en" version="2.1">
 	<context>
 		<name>policy</name>
 		<message>
-			<location filename="com.deepin.api.device.unblock-bluetooth-devices!message" line="0" />
+			<location filename="com.deepin.api.device.unblock-bluetooth-devices!message" line="0"/>
 			<source>Authentication is required to unblock bluetooth devices</source>
-			<translation type="unfinished" />
+			<translation>Authentication is required to unblock bluetooth devices</translation>
 		</message>
 		<message>
-			<location filename="com.deepin.api.device.unblock-bluetooth-devices!description" line="0" />
+			<location filename="com.deepin.api.device.unblock-bluetooth-devices!description" line="0"/>
 			<source>Unblock bluetooth devices</source>
-			<translation type="unfinished" />
+			<translation>Unblock bluetooth devices</translation>
 		</message>
 	</context>
 </TS>
--- misc/ts/com.deepin.api.locale-helper.policy/policy_pt.ts
+++ misc/ts/com.deepin.api.locale-helper.policy/policy_pt.ts
@@ -4,12 +4,12 @@
 		<message>
 			<location filename="com.deepin.api.locale-helper.manage-locale!message" line="0"/>
 			<source>Authentication is required to switch language</source>
-			<translation>A autenticação é obrigatória para mudar o idioma</translation>
+			<translation>A autenticação é necessária para alterar o idioma</translation>
 		</message>
 		<message>
 			<location filename="com.deepin.api.locale-helper.manage-locale!description" line="0"/>
 			<source>Manage your system locale</source>
-			<translation>Gerir o idioma do seu sistema</translation>
+			<translation>Gerir a localização do seu sistema</translation>
 		</message>
 	</context>
 </TS>
