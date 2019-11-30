0ac389fe9f917d0e58a93a5280397021cf8238ef
electricface <songwentai@deepin.com>
2019-11-30 10:09:32 +0800

    chore(dde-open): use pkg.deepin.io/lib/log

--- dde-open/main.go
+++ dde-open/main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"flag"
+	"errors"
 	"fmt"
-	"log"
 	"net/url"
 	"os"
 	"path/filepath"
@@ -11,103 +10,105 @@ import (
 	"github.com/linuxdeepin/go-dbus-factory/com.deepin.sessionmanager"
 	"pkg.deepin.io/gir/gio-2.0"
 	"pkg.deepin.io/lib/dbus1"
+	"pkg.deepin.io/lib/log"
 )
 
-var versionFlag bool
-
-func init() {
-	log.SetFlags(log.Lshortfile)
-	flag.BoolVar(&versionFlag, "version", false, "show version")
-}
+var logger = log.NewLogger("dde-open")
 
 func main() {
-	flag.Parse()
-	if versionFlag {
-		fmt.Println("0.0.1")
-		return
+	if len(os.Args) != 2 {
+		fmt.Println("usage: dde-open { file | URL }")
+		os.Exit(1)
 	}
-
-	arg := flag.Arg(0)
-	if arg == "" {
-		log.Fatal("arg empty")
-	}
+	arg := os.Args[1]
 
 	u, err := url.Parse(arg)
 	if err != nil {
-		log.Fatal(err)
+		logger.Warningf("failed to parse url %q: %v", arg, err)
+		err = openFile(arg)
+
+	} else {
+		switch u.Scheme {
+		case "file":
+			err = openFile(u.Path)
+
+		case "":
+			err = openFile(arg)
+
+		default:
+			err = openScheme(u.Scheme, arg)
+		}
 	}
-
-	switch u.Scheme {
-	case "file":
-		openFile(u.Path)
-
-	case "":
-		openFile(arg)
-
-	default:
-		openScheme(u.Scheme, arg)
+	if err != nil {
+		logger.Warning("open failed:", err)
+		os.Exit(2)
 	}
 }
 
-func launchApp(desktopFile, filename string) {
+func launchApp(desktopFile, filename string) error {
 	sessionBus, err := dbus.SessionBus()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	startManager := sessionmanager.NewStartManager(sessionBus)
 	err = startManager.LaunchApp(dbus.FlagNoAutoStart, desktopFile, 0,
 		[]string{filename})
-	if err != nil {
-		log.Fatal(err)
-	}
+	return err
 }
 
-func openFile(filename string) {
-	log.Printf("openFile: %q\n", filename)
+func openFile(filename string) error {
+	logger.Debugf("openFile: %q", filename)
 	filename, err := filepath.Abs(filename)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	_, err = os.Stat(filename)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	file := gio.FileNewForPath(filename)
 	defer file.Unref()
 
 	fileInfo, err := file.QueryInfo(gio.FileAttributeStandardContentType, gio.FileQueryInfoFlagsNone, nil)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer fileInfo.Unref()
 	contentType := fileInfo.GetAttributeString(gio.FileAttributeStandardContentType)
 	if contentType == "" {
-		log.Fatal("failed to get file content type")
+		return errors.New("failed to get file content type")
 	}
 
 	appInfo := gio.AppInfoGetDefaultForType(contentType, false)
 	if appInfo == nil {
-		log.Fatal("failed to get appInfo")
+		return errors.New("failed to get appInfo")
 	}
 	defer appInfo.Unref()
 
 	dAppInfo := gio.ToDesktopAppInfo(appInfo)
 	desktopFile := dAppInfo.GetFilename()
-	log.Println("desktop file:", desktopFile)
-	launchApp(desktopFile, filename)
+	logger.Debug("desktop file:", desktopFile)
+	err = launchApp(desktopFile, filename)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func openScheme(scheme, url string) {
-	log.Printf("openScheme: %q, %q\n", scheme, url)
+func openScheme(scheme, url string) error {
+	logger.Debugf("openScheme: %q, %q", scheme, url)
 	appInfo := gio.AppInfoGetDefaultForUriScheme(scheme)
-
 	if appInfo == nil {
-		log.Fatal("failed to get appInfo")
+		return errors.New("failed to get appInfo")
 	}
+	defer appInfo.Unref()
 
 	dAppInfo := gio.ToDesktopAppInfo(appInfo)
 	desktopFile := dAppInfo.GetFilename()
-	log.Println("desktop file:", desktopFile)
-	launchApp(desktopFile, url)
-	appInfo.Unref()
+	logger.Debug("desktop file:", desktopFile)
+	err := launchApp(desktopFile, url)
+	if err != nil {
+		return err
+	}
+	return nil
 }
