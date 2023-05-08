089e529f1117335745c6052767f4be70855f3c3a
Access <chenhongtao@uniontech.com>
2023-05-08 13:15:14 +0800

    chore: add gen.go support (#94)
    Log: add gen.go support

---
+++ gen.go
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: 2018 - 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package api
+
+//go:generate go build -o target/ github.com/linuxdeepin/dde-api/device
+//go:generate go build -o target/ github.com/linuxdeepin/dde-api/graphic
+//go:generate go build -o target/ github.com/linuxdeepin/dde-api/locale-helper
+//go:generate go build -o target/ github.com/linuxdeepin/dde-api/thumbnailer
+//go:generate go build -o target/ github.com/linuxdeepin/dde-api/hans2pinyin
+//go:generate go build -o target/ github.com/linuxdeepin/dde-api/cursor-helper
+//go:generate go build -o target/ github.com/linuxdeepin/dde-api/gtk-thumbnailer
+//go:generate go build -o target/ github.com/linuxdeepin/dde-api/sound-theme-player
+//go:generate go build -o target/ github.com/linuxdeepin/dde-api/deepin-shutdown-sound
+//go:generate go build -o target/ github.com/linuxdeepin/dde-api/dde-open
+//go:generate go build -o target/ github.com/linuxdeepin/dde-api/adjust-grub-theme
+//go:generate go build -o target/ github.com/linuxdeepin/dde-api/image-blur
+//go:generate go build -o target/ github.com/linuxdeepin/dde-api/image-blur-helper
